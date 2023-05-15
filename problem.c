5bf741071deeed599b06c435385e1b3c5638c79c
cppNoteAntar <118151423+cppNoteAntar@users.noreply.github.com>
2023-05-15 17:50:45 +0600

    Add files via upload

---
+++ problem.c
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define st string
+
+int32_t main()  
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+        
+std::sort(): Sorts elements in a range.
+std::find(): Finds the first occurrence of a value in a range.
+std::reverse(): Reverses the order of elements in a range.
+std::accumulate(): Calculates the sum of elements in a range.
+std::max_element(): Finds the largest element in a range.
+std::min_element(): Finds the smallest element in a range.
+std::count(): Counts the occurrences of a value in a range.
+std::copy(): Copies elements from one range to another.
+std::unique(): Removes consecutive duplicate elements from a range.
+std::replace(): Replaces all occurrences of a value in a range with another value.
+std::fill(): Assigns a value to all elements in a range.
+std::transform(): Applies a function to a range and stores the result in another range.
+std::binary_search(): Checks if a value exists in a sorted range.
+std::partition(): Divides elements in a range into two groups based on a given condition.
+std::count_if(): Counts the number of elements that satisfy a given condition in a range.
+std::find_if(): Finds the first element that satisfies a given condition in a range.
+std::next_permutation(): Generates the next lexicographically greater permutation of a range.
+std::distance(): Computes the distance between two iterators.
+std::unique_copy(): Copies unique elements from a range to another range.
+std::rotate(): Rotates the order of elements in a range.
+    
+}
